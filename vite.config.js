235719b0ae7f1d6ac960bcf04a4a1056895e2520
rafiq <islam.md.rafiqul@itage.co.jp>
2025-07-24 14:11:27 +0900

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
-	plugins: [react()],
 	base: "/algo-notes/", // change to your repo name
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "src"), // Alias @ → src
+		},
+	},
+	server: {
+		port: 3020,
+	},
 });

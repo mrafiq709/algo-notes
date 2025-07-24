52e47614d7a7a46f3c2848d8446022722d2b666b
rafiq <islam.md.rafiqul@itage.co.jp>
2025-07-24 14:39:01 +0900

    fix config

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,7 @@
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeHighlight from "rehype-highlight";
-import "highlight.js/styles/github.css"; // or another style you like
+import "highlight.js/styles/github.css";
 
 export default function Post({ content }) {
 	return (
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
 	base: "/algo-notes/", // change to your repo name
 	plugins: [react()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "src"), // Alias @ → src
-		},
-	},
-	server: {
-		port: 3020,
-	},
 });

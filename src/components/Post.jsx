1f9738e3cab98f408f585eef8ebe4e9975593db9
rafiq <islam.md.rafiqul@itage.co.jp>
2025-07-24 14:26:07 +0900

    mark down updated

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,16 @@
 import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github.css"; // or another style you like
 
 export default function Post({ content }) {
-	return <ReactMarkdown>{content}</ReactMarkdown>;
+	return (
+		<div className="markdown-body">
+			<ReactMarkdown
+				children={content}
+				remarkPlugins={[remarkGfm]}
+				rehypePlugins={[rehypeHighlight]}
+			/>
+		</div>
+	);
 }
